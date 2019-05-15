472d208b4cde6802a226492445e213f15403d9eb
Siddhartha Modur <publicinternetemail@gmail.com>
2019-05-15 02:49:08 -0400

    Finished Parsing Code

--- STVE/STVE/Meal.swift
+++ STVE/STVE/Meal.swift
@@ -4,16 +4,11 @@
 import Foundation
 import Kanna
 
-struct menuItem {
-	var dish: String
-	var description: String
-}
 
 class Menu {
 	var meal: String
-	var startTime: String;
-	var endTime: String;
-	var items: [String];
+	var timings: DateInterval
+	var items: [MenuItem];
 
 	init(menu: XMLElement) throws {
 		guard let h2 = menu.xpath("//h2@[class='panel__title site-panel__daypart-panel-title']").first else {
@@ -22,10 +17,46 @@ class Menu {
 		guard let mealtime = h2.text else {
 			throw ParsingError.HTMLError
 		}
+		guard let time_block = menu.xpath("//div@[class=\"site-panel__daypart-tim\"']").first else {
+			throw ParsingError.HTMLError
+		}
+		guard let timestr = time_block.text else {
+			throw ParsingError.HTMLError
+		}
+		let times = timestr.components(separatedBy: " - ")
+		parseTimeInfo(start: matches[0], end: matches[1])
 		getMenuItems(html: menu)
 	}
 	
+	func parseTimeInfo(start: String, end: String) {
+		let date = NSDate()
+		let fillVals = [.Year, .Month, .Day]
+		let startComp = NSCalendar.currentCalendar().components(fillVals, fromDate: date)
+		let endComp = NSCalendar.currentCalendar().components(fillVals, fromDate: date)
+		let startDate = parseDate(timeStr: start, template: startComp)
+		let endDate = parseDate(timeStr: end, template: endComp)
+		timings = DateInterval(start: startDate, end: endDate)
+	}
+	
+	func parseDate(timeStr: String, template: DateComponents) -> Date {
+		let comp = date.components(separatedBy: " ")
+		let time = comp[0].components(separatedBy: ":"); 
+		let period = comp[1]
+		if let hr = (Int(time[0]))? {
+			if (period == "pm") {
+				hr += 12
+			}
+			template.hour = hr
+		}
+		if let min = (Int(time[1]))? {
+			template.minute = min
+		}
+		return Calendar.current.date(from: template)
+	}
+	
 	func getMenuItems(html: XMLElement) {
-		
+		for item in html.xpath("//div@[class='site-panel__daypart-item-container']") {
+			items.append(MenuItem(item));
+		}
 	}
 }
